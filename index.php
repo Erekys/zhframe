c0e0379944077dab8a579b2adeeaf6432f92f7dd
zhouhongfu <zhouhongfu@wanglibank.com>
2017-10-14 13:46:52 +0800

    base frame

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erekys
+ * Date: 2017/10/14
+ * Time: 13:37
+ */
+// 应用目录为当前目录
+define('APP_PATH', __DIR__.'/');
+// 开启调试模式
+define('APP_DEBUG', true);
+// 网站根URL
+define('APP_URL', 'http://localhost/s');
+// 加载框架
+require './zhframe/start.php';
